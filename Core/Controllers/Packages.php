04a4a314d0ecf984d2e210aca1781ccc9c4c5ba4
root <root@localhost.localdomain>
2021-12-29 07:50:12 -0700

    UPDATE

--- Core/Controllers/Packages.php
+++ Core/Controllers/Packages.php
@@ -128,7 +128,7 @@
             // $output = shell_exec($command);
             $command = escapeshellcmd('sudo /usr/bin/python3 DebugTr.py');
             $output = shell_exec($command);
-            // $Result = $output;
+            $Result = $output;
             // pclose(popen("cd /var/www/html/BBINCO/TV/Core/Controllers && python3 DebugTr.py'","r"));
             // $your_command = 'cd /var/www/html/BBINCO/TV/Core/Controllers && python3 DebugTr.py';
             // shell_exec( $your_command . "> /dev/null 2>/dev/null &" );
