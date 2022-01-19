2928f72948175be227bc30bc1bf44a8d0ab4ee51
root <root@localhost.localdomain>
2022-01-19 07:56:53 -0700

    BBINCOTV

--- Core/Controllers/CreateLog.py
+++ Core/Controllers/CreateLog.py
@@ -23,11 +23,7 @@ try:
 except:
     file = open("/var/www/html/BBINCO/TV/Core/Controllers/logInformir.txt", "w")
     file.write(log + parametro[0] + ' | ' + parametro[1] + ' | ' + parametro[2] + ' | ' + parametro[3]+ '\n')
-<<<<<<< HEAD
     file.close()
 
 
 
-=======
-    file.close()
->>>>>>> aa2bc7a2b2e456761f5cc230835fc0a34aa476e1
